b09488cad3f4b17c7bdd41967312697e7a13e3b5
shayan <shayan4shayan@gmail.com>
2019-11-09 02:59:08 +0330

    model migration added.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class State(models.Model):
+    time_span = models.IntegerField(blank=False, null=False)
+    is_weekend = models.BooleanField(blank=False, null=False)
+
+
+class Record(models.Model):
+    action = models.CharField(max_length=50, blank=False, null=False)
+    reward = models.IntegerField(blank=False, null=False)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
